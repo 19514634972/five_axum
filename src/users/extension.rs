7d088a4f38b36e79d902d20737c2610a9b5a2411
wzf <1490216271@qq.com>
2025-02-17 12:45:55 +0800

    加全局中间件

--- src/users/extension.rs
+++ src/users/extension.rs
@@ -2,9 +2,9 @@ use std::sync::Arc;
 //扩展共享数据
 use axum::{Extension, Router};
 use axum::extract::{Request, State};
-use axum::http::{Response, StatusCode};
-use axum::middleware::Next;
-use axum::response::IntoResponse;
+use axum::http:: StatusCode;
+use axum::middleware::{from_fn, Next};
+use axum::response::{IntoResponse,Response};
 use axum::routing::{get, post, put, delete};
 
 pub struct AppSate{
@@ -29,12 +29,12 @@ impl AppSate{
 pub  fn create_routes() ->Router{
     //Arc<AppState>
   let db_data=Arc::new(AppSate::new());
-    let share=Router::new()
+   Router::new()
         .route("/shared-data-1",get(share_data_1))
         .route("/shared-data-2",get(share_data_2))
         .layer(Extension(db_data))
-        .route_layer()  //路由中间
-    share
+        .route_layer(from_fn(customer_middleware))
+
 }
 
 async fn share_data_1(Extension(db):Extension<Arc<AppSate>>)->String{
@@ -45,7 +45,9 @@ async fn share_data_2(Extension(db):Extension<Arc<AppSate>>)->String{
     format!("share_data_2:{}",db.get_conn())
 }
 
-async fn customer_middleware(req:Request,next: Next)->Result<Response<T>,StatusCode>{
-
-    Ok("Hi".into_response())
+async fn customer_middleware(req:Request,next: Next)->Result<Response,StatusCode>{
+    if req.headers().get("X-custom-header").is_none(){
+        return Err(StatusCode::BAD_REQUEST)
+    }
+    Ok(next.run(req).await)
 }
